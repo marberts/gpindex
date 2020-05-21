2126023e47b3e8e1b5cbb05a3b20c796396ad6d3
Steve Martin <stevemartin041@gmail.com>
2020-05-21 16:01:28 -0400

    Added some datasets

--- R/contribution.R
+++ R/contribution.R
@@ -1,19 +1,22 @@
 contribution_geometric <- function (p1, p0, q1, q0, pb, qb, type, na.rm = FALSE) {
   check_index_arguments(p1, p0, q1, q0, pb, qb, type, na.rm)
   type <- match.arg(type, types$geometric_index_types)
-  weights_g2a(p1 / p0, index_weights(p1, p0, q1, q0, pb, qb, type), na.rm) * p1 / p0
+  w <- weights_g2a(p1 / p0, index_weights(p1, p0, q1, q0, pb, qb, type), na.rm)
+  w / sum(w) * p1 / p0
 }
 
 contribution_harmonic<- function (p1, p0, q1, q0, pb, qb, type, na.rm = FALSE) {
   check_index_arguments(p1, p0, q1, q0, pb, qb, type, na.rm)
   type <- match.arg(type, types$harmonic_index_types)
-  weights_h2a(p1 / p0, index_weights(p1, p0, q1, q0, pb, qb, type), na.rm) * p1 / p0
+  w <- weights_h2a(p1 / p0, index_weights(p1, p0, q1, q0, pb, qb, type), na.rm) 
+  w / sum(w) * p1 / p0
 }
 
 contribution_fisher <- function (p1, p0, q1, q0, na.rm = FALSE) {
   check_index_arguments(p1, p0, q1, q0, na.rm = na.rm)
   Qf <- index_fisher(q1, q0, p1, p0, na.rm)
   Ql <- index_arithmetic(q1, q0, p1, p0, type = "Laspeyres", na.rm = na.rm)
-  (Qf / (Qf + Ql) * index_weights(p1, p0, q1, q0, type = "Laspeyres") + 
-    Ql / (Qf + Ql) * index_weights(p1, p0, q1, q0, type = "HybridPaasche")) * p1 / p0
+  wl <- index_weights(p1, p0, q1, q0, type = "Laspeyres")
+  wp <- index_weights(p1, p0, q1, q0, type = "HybridPaasche")
+  (Qf / (Qf + Ql) * wl + Ql / (Qf + Ql) * wp) * p1 / p0
 }
