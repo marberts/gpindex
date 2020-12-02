33484fc054aed5592a9c0d9cc75fbab15fd83cef
Steve Martin <stevemartin041@gmail.com>
2020-12-01 23:54:17 -0500

    Getting ready to submit to CRAN

--- R/3_price_indexes.R
+++ R/3_price_indexes.R
@@ -12,7 +12,7 @@ index_weights <- function(type = c("Carli", "Jevons", "Coggeshall",
     match.arg(type),
     Carli = ,
     Jevons = ,
-    Coggeshall = function(p0) structure(rep(1, length(p0)), names = names(p0)),
+    Coggeshall = function(p0) replace(p0, values = 1), # keep attributes
     Dutot = function(p0) p0,
     Young = function(pb, qb) pb * qb,
     Lowe = function(p0, qb) p0 * qb,
