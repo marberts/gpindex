4580d352c84101b680b63e1a6dfdf891f7082b18
Steve Martin <stevemartin041@gmail.com>
2020-12-29 10:15:17 -0500

    Added base_price()

--- R/5_utilities.R
+++ R/5_utilities.R
@@ -1,18 +1,26 @@
-#---- Back price ----
-back_price <- function(x, period, product = rep(1, length(x))) {
-  stopifnot("All arguments must be atomic vectors" = all_atomic(x, period, product),
-            "All arguments must be the same length" = all_same_length(x, period, product))
-  if (!length(x)) return(x[0])
-  offset <- function(x) x[c(1L, seq_len(length(x) - 1))]
-  period <- as.factor(period)
-  price <- split(x, period)
-  product <- split(product, period)
-  warn <- any(vapply(product, anyDuplicated, numeric(1)) > 0)
-  if (warn) {
-    warning("There are duplicated period-product pairs; back price not defined") 
+#---- Price utilities----
+offset_price <- function(type = c("back", "base")) {
+  type <- match.arg(type)
+  # return function
+  function(x, period, product = rep(1, length(x))) {
+    stopifnot("All arguments must be atomic vectors" = all_atomic(x, period, product),
+              "All arguments must be the same length" = all_same_length(x, period, product))
+    if (!length(x)) return(x[0])
+    offset <- function(x) x[c(1L, if (type == "back") seq_len(length(x) - 1))]
+    period <- as.factor(period)
+    price <- split(x, period)
+    product <- split(product, period)
+    warn <- any(vapply(product, anyDuplicated, numeric(1)) > 0)
+    if (warn) {
+      warning("There are duplicated period-product pairs; back price not defined") 
+    }
+    matches <- Map(match, product, offset(product), incomparables = NA)
+    res <- unsplit(Map(`[`, offset(price), matches), period)
+    attributes(res) <- attributes(x) # unsplit mangles attributes
+    res
   }
-  matches <- Map(match, product, offset(product), incomparables = NA)
-  res <- unsplit(Map(`[`, offset(price), matches), period)
-  attributes(res) <- attributes(x) # unsplit mangles attributes
-  res
 }
+
+back_price <- offset_price("back")
+
+base_price <- offset_price("base")
--- tests/test_utilities.R
+++ tests/test_utilities.R
@@ -7,9 +7,11 @@ stopifnot(
   exprs = {
     length(back_price(NULL, NULL, NULL)) == 0
     back_price(1:4, rep(1, 4), 1:4) == 1:4
+    base_price(1:4, rep(1, 4), 1:4) == 1:4
     identical(back_price(matrix(1:4), rep(1, 4), 1:4), matrix(1:4))
     is.na(back_price(1:4, factor(rep(1, 4), levels = 0:1), 1:4))
     back_price(1:4, 1:4, rep(1, 4)) == c(1, 1, 2, 3)
+    base_price(1:4, 1:4, rep(1, 4)) == c(1, 1, 1, 1)
     back_price(1:4, 1:4) == c(1, 1, 2, 3)
     back_price(1:4, factor(1:4, levels = 4:1), rep(1, 4)) == c(2, 3, 4, 4)
     identical(price / back_price(price, period, id), 
