70cdcce60fadc427f69c13bfdd9fa6af7236ac9d
Steve Martin <stevemartin041@gmail.com>
2021-01-29 00:27:07 -0500

    Removed most argument checking

--- R/2_weights.R
+++ R/2_weights.R
@@ -12,11 +12,11 @@ weights_transmute <- function(r, s) {
 
 #---- Factor weights  ----
 weights_factor <- function(r) {
-  stopifnot("'r' must be a finite length 1 numeric" = is_number(r))
+  if (!is_number(r)) {
+    stop("'r' must be a finite length 1 numeric")
+  }
   # return function
   function(x, w = rep(1, length(x))) {
-    stopifnot("'x' and 'w' must be numeric vectors" = all_numeric(x, w),
-              "'x' and 'w' must be the same length" = all_same_length(x, w))
     res <- w * x %^% r
     # make sure NAs propagate to ensure chaining works correctly with NAs in x
     replace(res, if (r == 0) is.na(x) & !is.na(w), NA)
@@ -45,15 +45,15 @@ contributions_geometric <- contributions(0)
 contributions_harmonic <- contributions(-1)
 
 contributions_nested <- function(r1, r2, w1 = rep(1L, length(r2))) {
-  stopifnot("'r1' must be a finite length 1 numeric" = is_number(r1),
-            "'r2' and 'w1' must be a numeric vectors" = all_numeric(r2, w1),
-            "'r2' and 'w1' must be the same length" = all_same_length(r2, w1))
+  # stopifnot("'r1' must be a finite length 1 numeric" = is_number(r1),
+  #           "'r2' and 'w1' must be a numeric vectors" = all_numeric(r2, w1),
+  #           "'r2' and 'w1' must be the same length" = all_same_length(r2, w1))
   function(x, w = rep(list(rep(1, length(x))), length(r2))) {
-    stopifnot("'x' must be a numeric vector" = is.numeric(x),
-              "'w' must be a list" = is.list(w),
-              "'r2' and 'w' must be the same length" = all_same_length(r2, w),
-              "Each element of 'w' must be a numeric vector" = do.call(all_numeric, w),
-              "Each element of 'w' must be the same length as 'x'" = do.call(all_same_length, c(list(x), w)))
+    # stopifnot("'x' must be a numeric vector" = is.numeric(x),
+    #           "'w' must be a list" = is.list(w),
+    #           "'r2' and 'w' must be the same length" = all_same_length(r2, w),
+    #           "Each element of 'w' must be a numeric vector" = do.call(all_numeric, w),
+    #           "Each element of 'w' must be the same length as 'x'" = do.call(all_same_length, c(list(x), w)))
     means <- lapply(r2, mean_generalized)
     contribs <- lapply(r2, contributions)
     ind <- seq_along(r2)
--- R/5_utilities.R
+++ R/5_utilities.R
@@ -3,8 +3,8 @@ offset_price <- function(type = c("back", "base")) {
   type <- match.arg(type)
   # return function
   function(x, period, product = rep(1, length(x))) {
-    stopifnot("All arguments must be atomic vectors" = all_atomic(x, period, product),
-              "All arguments must be the same length" = all_same_length(x, period, product))
+    # stopifnot("All arguments must be atomic vectors" = all_atomic(x, period, product),
+    #           "All arguments must be the same length" = all_same_length(x, period, product))
     if (!length(x)) return(x[0])
     offset <- function(x) x[c(1L, if (type == "back") seq_len(length(x) - 1))]
     period <- as.factor(period)
