e5449aaf1a45fa7ca4c24ba01026215bdd9c5754
marberts <stevemartin041@gmail.com>
2021-04-20 07:06:26 -0400

    price offset functions now correctly coerced product to a factor

--- R/5_utilities.R
+++ R/5_utilities.R
@@ -2,7 +2,7 @@
 offset_price <- function(type = c("back", "base")) {
   type <- match.arg(type)
   # return function
-  function(x, period, product = rep(1, length(x))) {
+  function(x, period, product = gl(1, length(x))) {
     if (!same_length(x, period, product)) {
       stop("All arguments must be the same length")
     }
@@ -10,7 +10,7 @@ offset_price <- function(type = c("back", "base")) {
     offset <- function(x) x[c(1L, if (type == "back") seq_len(length(x) - 1))]
     period <- as.factor(period)
     price <- split(x, period)
-    product <- split(product, period)
+    product <- split(as.factor(product), period)
     warn <- any(vapply(product, anyDuplicated, numeric(1)) > 0)
     if (warn) {
       warning("There are duplicated period-product pairs") 
